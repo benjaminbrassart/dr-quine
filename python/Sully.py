df28cf255a9c98741577b49b63222df11cbd4c8a
Benjamin Brassart <bbrassar@student.42.fr>
2023-11-27 17:48:03 +0100

    Add python sully

---
+++ python/Sully.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import os
+
+FORMAT = "#!/usr/bin/env python3{1}{1}import os{1}{1}FORMAT = {0}{2}{0}{1}{1}def duplicate(i):{1}    with open(f{0}Sully_{{i}}.py{0}, {0}w{0}) as f:{1}        print(FORMAT.format(chr(34), chr(10), FORMAT, i), file=f){1}{1}def run(i):{1}    os.system(f{0}/usr/bin/env python3 Sully_{{i}}.py{0}) == 0 or exit(1){1}{1}if __name__ == {0}__main__{0}:{1}    i = {3}{1}    i -= 1{1}{1}    duplicate(i){1}    if i > 0:{1}        run(i)"
+
+def duplicate(i):
+    with open(f"Sully_{i}.py", "w") as f:
+        print(FORMAT.format(chr(34), chr(10), FORMAT, i), file=f)
+
+def run(i):
+    os.system(f"/usr/bin/env python3 Sully_{i}.py") == 0 or exit(1)
+
+if __name__ == "__main__":
+    i = 5
+    i -= 1
+
+    duplicate(i)
+    if i > 0:
+        run(i)
