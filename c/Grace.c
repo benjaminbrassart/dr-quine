acf6c0a2fb85de59eaef3dfc2e87784301f8e883
Benjamin Brassart <bbrassar@student.42.fr>
2023-11-27 07:44:07 +0100

    Use 3 macros for Grace, and add missing comment

--- c/Grace.c
+++ c/Grace.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-#define PRINT(file, format) fprintf(file, format, 10, 34, format)
-#define MAIN() int main(void){FILE *f=fopen("Grace_kid.c", "w");if(f==0){fputs("cannot write to Grace_kid.c", stderr);return 1;}char const fmt[]="#include <stdio.h>%1$c#define PRINT(file, format) fprintf(file, format, 10, 34, format)%1$c#define MAIN() int main(void){FILE *f=fopen(%2$cGrace_kid.c%2$c, %2$cw%2$c);if(f==0){fputs(%2$ccannot write to Grace_kid.c%2$c, stderr);return 1;}char const fmt[]=%2$c%3$s%2$c;PRINT(f, fmt);fclose(f);}%1$c%1$cMAIN()%1$c";PRINT(f, fmt);fclose(f);}
+/*
+	some comment idk
+*/
+#define FORMAT "#include <stdio.h>%1$c/*%1$c%2$csome comment idk%1$c*/%1$c#define FORMAT %3$c%4$s%3$c%1$c#define PRINT(File, Format) fprintf(File, Format, 10, 9, 34, Format)%1$c#define MAIN() int main(void) { FILE *f = fopen(%3$cGrace_kid.c%3$c, %3$cw%3$c); if (f == NULL) { fputs(%3$ccannot write to Grace_kid.c%3$c, stderr); return 1; } PRINT(f, FORMAT); fclose(f); }%1$c%1$cMAIN()%1$c"
+#define PRINT(File, Format) fprintf(File, Format, 10, 9, 34, Format)
+#define MAIN() int main(void) { FILE *f = fopen("Grace_kid.c", "w"); if (f == NULL) { fputs("cannot write to Grace_kid.c", stderr); return 1; } PRINT(f, FORMAT); fclose(f); }
 
 MAIN()
