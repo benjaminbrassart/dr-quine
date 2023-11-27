e6bb0f4af444f4037930091515f42a35cec6ecba
Benjamin Brassart <bbrassar@student.42.fr>
2023-11-27 08:49:18 +0100

    Finish Sully

--- c/Sully.c
+++ c/Sully.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define FORMAT "#include <stdio.h>%1$c#include <stdlib.h>%1$c%1$c#define FORMAT %3$c%4$s%3$c%1$c%1$cstatic char buffer[256];%1$c%1$cstatic void run(int i)%1$c{%1$c%2$csnprintf;%1$c%2$csystem(buffer);%1$c}%1$c%1$cstatic void compile(int i)%1$c{%1$c%2$csnprintf;%1$c%2$csystem(buffer);%1$c}%1$c%1$cstatic void duplicate(int i)%1$c{%1$c%2$csnprintf;%1$c%1$c%2$cFILE *f = fopen;%1$c%2$cfprintf(f, FORMAT, 10, 9, 34, FORMAT, i);%1$c%2$cfclose(f);%1$c}%1$c%1$cint main(void)%1$c{%1$c%2$cint i = %5$d;%1$c%1$c%2$ci -= 1;%1$c%2$cduplicate(i);%1$c%2$ccompile(i);%1$c%2$cif (i > 0)%1$c%2$c%2$crun(i);%1$c}%1$c"
+#define FORMAT "#include <stdio.h>%1$c#include <stdlib.h>%1$c%1$c#define FORMAT %3$c%4$s%3$c%1$c%1$cstatic char buffer[256];%1$c%1$cstatic void run(int i)%1$c{%1$c%2$csnprintf(buffer, sizeof(buffer), %3$c./Sully_%%d%3$c, i);%1$c%2$csystem(buffer) == 0 || (exit(1), 0);%1$c}%1$c%1$cstatic void compile(int i)%1$c{%1$c%2$csnprintf(buffer, sizeof(buffer), %3$ccc Sully_%%1$d.c -o Sully_%%1$d%3$c, i);%1$c%2$csystem(buffer) == 0 || (exit(1), 0);%1$c}%1$c%1$cstatic void duplicate(int i)%1$c{%1$c%2$csnprintf(buffer, sizeof(buffer), %3$cSully_%%d.c%3$c, i);%1$c%1$c%2$cFILE *f = fopen(buffer, %3$cw%3$c);%1$c%2$cfprintf(f, FORMAT, 10, 9, 34, FORMAT, i);%1$c%2$cfclose(f);%1$c}%1$c%1$cint main(void)%1$c{%1$c%2$cint i = %5$d;%1$c%1$c%2$ci -= 1;%1$c%2$cduplicate(i);%1$c%2$ccompile(i);%1$c%2$cif (i > 0)%1$c%2$c%2$crun(i);%1$c}%1$c"
 
 static char buffer[256];
 
