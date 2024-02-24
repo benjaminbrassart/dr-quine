556a2c3b782936249f4a45aaa577d54d5b1f54b7
Benjamin Brassart <bbrassar@student.42.fr>
2024-02-24 21:10:35 +0100

    Change second comment for perl colleen

--- perl/Colleen.pl
+++ perl/Colleen.pl
@@ -9,9 +9,9 @@ use warnings;
 
 sub colleen {
 	#
-	#	it's just like me, weird but useful i guess
+	#	fancy some unicode? 🫠
 	#
-	my $s = '#!/usr/bin/env perl%1$c%1$cuse strict;%1$cuse warnings;%1$c%1$c#%1$c#%2$cwhat the hell kinda language is this%1$c#%1$c%1$csub colleen {%1$c%2$c#%1$c%2$c#%2$cit%3$cs just like me, weird but useful i guess%1$c%2$c#%1$c%2$cmy $s = %3$c%4$s%3$c;%1$c%1$c%2$cprintf "$s", 10, 9, 39, "$s";%1$c}%1$c%1$csub main {%1$c%2$ccolleen;%1$c}%1$c%1$cmain();%1$c';
+	my $s = '#!/usr/bin/env perl%1$c%1$cuse strict;%1$cuse warnings;%1$c%1$c#%1$c#%2$cwhat the hell kinda language is this%1$c#%1$c%1$csub colleen {%1$c%2$c#%1$c%2$c#%2$cfancy some unicode? 🫠%1$c%2$c#%1$c%2$cmy $s = %3$c%4$s%3$c;%1$c%1$c%2$cprintf "$s", 10, 9, 39, "$s";%1$c}%1$c%1$csub main {%1$c%2$ccolleen;%1$c}%1$c%1$cmain();%1$c';
 
 	printf "$s", 10, 9, 39, "$s";
 }
