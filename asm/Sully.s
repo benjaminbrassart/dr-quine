5754120778f60da2025aaf4ba5f7cfc05e6cd862
Benjamin Brassart <bbrassar@student.42.fr>
2025-06-09 15:56:10 +0200

    Fix Sully logic for C and assembly

--- asm/Sully.s
+++ asm/Sully.s
@@ -8,15 +8,9 @@ global main
 main:
 	mov qword [i], 5
 
-	add rsp, -8
-		mov rdi, file_name
-		mov rsi, root_name
-		call strcmp
-	add rsp, 8
+	cmp qword [i], -1
+	jle .return
 
-	cmp rax, 0
-	je .duplicate
-	dec qword [i]
 .duplicate:
 	add rsp, -8
 		mov rdi, buffer
@@ -41,8 +35,10 @@ main:
 		mov r8, 34
 		mov r9, format
 		add rsp, -8
+		dec qword [i]
 		push qword [i]
 		call fprintf
+		inc qword [i]
 		add rsp, 16
 
 	pop rax
@@ -65,9 +61,6 @@ main:
 		mov rdi, buffer
 		call system
 	add rsp, 8
-
-	cmp qword [i], 1
-	jl .return
 
 .run:
 	add rsp, -8
@@ -95,22 +88,18 @@ i:
 	resq 1
 section .data
 align 16
-file_name:
-	db __FILE__, 0
-root_name:
-	db "Sully.s", 0
 format_run:
 	db "./Sully_%lld", 0
 format_duplicate:
 	db "Sully_%lld.s", 0
 format_compile:
 	db "nasm -f elf64 -g Sully_%1$lld.s -o Sully_%1$lld.o"
-	db "; "
+	db " && "
 	db "cc -lc -no-pie Sully_%1$lld.o -o Sully_%1$lld"
-	db "; "
+	db " && "
 	db "rm -f Sully_%1$lld.o"
 	db 0
 open_mode:
 	db "w", 0
 format:
-	db "; vim: set ft=nasm%1$c; vim: nomodeline%1$c%1$cextern strcmp, snprintf, fopen, fprintf, fclose, system%1$c%1$csection .text%1$cglobal main%1$cmain:%1$c%2$cmov qword [i], %5$lld%1$c%1$c%2$cadd rsp, -8%1$c%2$c%2$cmov rdi, file_name%1$c%2$c%2$cmov rsi, root_name%1$c%2$c%2$ccall strcmp%1$c%2$cadd rsp, 8%1$c%1$c%2$ccmp rax, 0%1$c%2$cje .duplicate%1$c%2$cdec qword [i]%1$c.duplicate:%1$c%2$cadd rsp, -8%1$c%2$c%2$cmov rdi, buffer%1$c%2$c%2$cmov rsi, BUFFER_SIZE%1$c%2$c%2$cmov rdx, format_duplicate%1$c%2$c%2$cmov rcx, qword [i]%1$c%2$c%2$ccall snprintf%1$c%2$cadd rsp, 8%1$c%1$c%2$cadd rsp, -8%1$c%2$c%2$cmov rdi, buffer%1$c%2$c%2$cmov rsi, open_mode%1$c%2$c%2$ccall fopen%1$c%2$cadd rsp, 8%1$c%1$c%2$cpush rax%1$c%1$c%2$c%2$cmov rdi, rax%1$c%2$c%2$cmov rsi, format%1$c%2$c%2$cmov rdx, 10%1$c%2$c%2$cmov rcx, 9%1$c%2$c%2$cmov r8, 34%1$c%2$c%2$cmov r9, format%1$c%2$c%2$cadd rsp, -8%1$c%2$c%2$cpush qword [i]%1$c%2$c%2$ccall fprintf%1$c%2$c%2$cadd rsp, 16%1$c%1$c%2$cpop rax%1$c%1$c%2$cadd rsp, -8%1$c%2$c%2$cmov rdi, rax%1$c%2$c%2$ccall fclose%1$c%2$cadd rsp, 8%1$c%1$c.compile:%1$c%2$cadd rsp, -8%1$c%2$c%2$cmov rdi, buffer%1$c%2$c%2$cmov rsi, BUFFER_SIZE%1$c%2$c%2$cmov rdx, format_compile%1$c%2$c%2$cmov rcx, qword [i]%1$c%2$c%2$ccall snprintf%1$c%2$cadd rsp, 8%1$c%1$c%2$cadd rsp, -8%1$c%2$c%2$cmov rdi, buffer%1$c%2$c%2$ccall system%1$c%2$cadd rsp, 8%1$c%1$c%2$ccmp qword [i], 1%1$c%2$cjl .return%1$c%1$c.run:%1$c%2$cadd rsp, -8%1$c%2$c%2$cmov rdi, buffer%1$c%2$c%2$cmov rsi, BUFFER_SIZE%1$c%2$c%2$cmov rdx, format_run%1$c%2$c%2$cmov rcx, qword [i]%1$c%2$c%2$ccall snprintf%1$c%2$cadd rsp, 8%1$c%1$c%2$cadd rsp, -8%1$c%2$c%2$cmov rdi, buffer%1$c%2$c%2$ccall system%1$c%2$cadd rsp, 8%1$c%1$c.return:%1$c%2$cmov rax, 0%1$c%2$cret%1$c%1$csection .bss%1$cbuffer:%1$c%2$cresb 1024%1$cBUFFER_SIZE equ $ - buffer%1$ci:%1$c%2$cresq 1%1$csection .data%1$calign 16%1$cfile_name:%1$c%2$cdb __FILE__, 0%1$croot_name:%1$c%2$cdb %3$cSully.s%3$c, 0%1$cformat_run:%1$c%2$cdb %3$c./Sully_%%lld%3$c, 0%1$cformat_duplicate:%1$c%2$cdb %3$cSully_%%lld.s%3$c, 0%1$cformat_compile:%1$c%2$cdb %3$cnasm -f elf64 -g Sully_%%1$lld.s -o Sully_%%1$lld.o%3$c%1$c%2$cdb %3$c; %3$c%1$c%2$cdb %3$ccc -lc -no-pie Sully_%%1$lld.o -o Sully_%%1$lld%3$c%1$c%2$cdb %3$c; %3$c%1$c%2$cdb %3$crm -f Sully_%%1$lld.o%3$c%1$c%2$cdb 0%1$copen_mode:%1$c%2$cdb %3$cw%3$c, 0%1$cformat:%1$c%2$cdb %3$c%4$s%3$c, 0%1$c", 0
+	db "; vim: set ft=nasm%1$c; vim: nomodeline%1$c%1$cextern strcmp, snprintf, fopen, fprintf, fclose, system%1$c%1$csection .text%1$cglobal main%1$cmain:%1$c%2$cmov qword [i], %5$lld%1$c%1$c%2$ccmp qword [i], -1%1$c%2$cjle .return%1$c%1$c%2$cadd rsp, -8%1$c%2$c%2$cmov rdi, buffer%1$c%2$c%2$cmov rsi, BUFFER_SIZE%1$c%2$c%2$cmov rdx, format_duplicate%1$c%2$c%2$cmov rcx, qword [i]%1$c%2$c%2$ccall snprintf%1$c%2$cadd rsp, 8%1$c%1$c%2$cadd rsp, -8%1$c%2$c%2$cmov rdi, buffer%1$c%2$c%2$cmov rsi, open_mode%1$c%2$c%2$ccall fopen%1$c%2$cadd rsp, 8%1$c%1$c%2$cpush rax%1$c%1$c%2$c%2$cmov rdi, rax%1$c%2$c%2$cmov rsi, format%1$c%2$c%2$cmov rdx, 10%1$c%2$c%2$cmov rcx, 9%1$c%2$c%2$cmov r8, 34%1$c%2$c%2$cmov r9, format%1$c%2$c%2$cadd rsp, -8%1$c%2$c%2$cdec qword [i]%1$c%2$c%2$cpush qword [i]%1$c%2$c%2$ccall fprintf%1$c%2$c%2$cinc qword [i]%1$c%2$c%2$cadd rsp, 16%1$c%1$c%2$cpop rax%1$c%1$c%2$cadd rsp, -8%1$c%2$c%2$cmov rdi, rax%1$c%2$c%2$ccall fclose%1$c%2$cadd rsp, 8%1$c%1$c.compile:%1$c%2$cadd rsp, -8%1$c%2$c%2$cmov rdi, buffer%1$c%2$c%2$cmov rsi, BUFFER_SIZE%1$c%2$c%2$cmov rdx, format_compile%1$c%2$c%2$cmov rcx, qword [i]%1$c%2$c%2$ccall snprintf%1$c%2$cadd rsp, 8%1$c%1$c%2$cadd rsp, -8%1$c%2$c%2$cmov rdi, buffer%1$c%2$c%2$ccall system%1$c%2$cadd rsp, 8%1$c%1$c.run:%1$c%2$cadd rsp, -8%1$c%2$c%2$cmov rdi, buffer%1$c%2$c%2$cmov rsi, BUFFER_SIZE%1$c%2$c%2$cmov rdx, format_run%1$c%2$c%2$cmov rcx, qword [i]%1$c%2$c%2$ccall snprintf%1$c%2$cadd rsp, 8%1$c%1$c%2$cadd rsp, -8%1$c%2$c%2$cmov rdi, buffer%1$c%2$c%2$ccall system%1$c%2$cadd rsp, 8%1$c%1$c.return:%1$c%2$cmov rax, 0%1$c%2$cret%1$c%1$csection .bss%1$cbuffer:%1$c%2$cresb 1024%1$cBUFFER_SIZE equ $ - buffer%1$ci:%1$c%2$cresq 1%1$csection .data%1$calign 16%1$cformat_run:%1$c%2$cdb %3$c./Sully_%%lld%3$c, 0%1$cformat_duplicate:%1$c%2$cdb %3$cSully_%%lld.s%3$c, 0%1$cformat_compile:%1$c%2$cdb %3$cnasm -f elf64 -g Sully_%%1$lld.s -o Sully_%%1$lld.o%3$c%1$c%2$cdb %3$c && %3$c%1$c%2$cdb %3$ccc -lc -no-pie Sully_%%1$lld.o -o Sully_%%1$lld%3$c%1$c%2$cdb %3$c && %3$c%1$c%2$cdb %3$crm -f Sully_%%1$lld.o%3$c%1$c%2$cdb 0%1$copen_mode:%1$c%2$cdb %3$cw%3$c, 0%1$cformat:%1$c%2$cdb %3$c%4$s%3$c, 0%1$c", 0
--- c/Sully.c
+++ c/Sully.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define FORMAT "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <string.h>%1$c%1$c#define FORMAT %3$c%4$s%3$c%1$c%1$cstatic char buffer[256];%1$c%1$cstatic void run(int i)%1$c{%1$c%2$csnprintf(buffer, sizeof(buffer), %3$c./Sully_%%d%3$c, i);%1$c%2$csystem(buffer) == 0 || (exit(1), 0);%1$c}%1$c%1$cstatic void compile(int i)%1$c{%1$c%2$csnprintf(buffer, sizeof(buffer), %3$ccc Sully_%%1$d.c -o Sully_%%1$d%3$c, i);%1$c%2$csystem(buffer) == 0 || (exit(1), 0);%1$c}%1$c%1$cstatic void duplicate(int i)%1$c{%1$c%2$csnprintf(buffer, sizeof(buffer), %3$cSully_%%d.c%3$c, i);%1$c%1$c%2$cFILE *f = fopen(buffer, %3$cw%3$c);%1$c%1$c%2$cfprintf(f, FORMAT, 10, 9, 34, FORMAT, i);%1$c%2$cfclose(f);%1$c}%1$c%1$cint main(void)%1$c{%1$c%2$cint i = %5$d;%1$c%1$c%2$cif (strcmp(__FILE__, %3$cSully.c%3$c) != 0)%1$c%2$c%2$ci -= 1;%1$c%1$c%2$cduplicate(i);%1$c%2$ccompile(i);%1$c%2$cif (i > 0)%1$c%2$c%2$crun(i);%1$c}%1$c"
+#define FORMAT "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <string.h>%1$c%1$c#define FORMAT %3$c%4$s%3$c%1$c%1$cstatic char buffer[256];%1$c%1$cstatic void run(int i)%1$c{%1$c%2$csnprintf(buffer, sizeof(buffer), %3$c./Sully_%%d%3$c, i);%1$c%2$csystem(buffer) == 0 || (exit(1), 0);%1$c}%1$c%1$cstatic void compile(int i)%1$c{%1$c%2$csnprintf(buffer, sizeof(buffer), %3$ccc Sully_%%1$d.c -o Sully_%%1$d%3$c, i);%1$c%2$csystem(buffer) == 0 || (exit(1), 0);%1$c}%1$c%1$cstatic void duplicate(int i)%1$c{%1$c%2$csnprintf(buffer, sizeof(buffer), %3$cSully_%%d.c%3$c, i);%1$c%1$c%2$cFILE *f = fopen(buffer, %3$cw%3$c);%1$c%1$c%2$cfprintf(f, FORMAT, 10, 9, 34, FORMAT, i - 1);%1$c%2$cfclose(f);%1$c}%1$c%1$cint main(void)%1$c{%1$c%2$cint i = %5$d;%1$c%1$c%2$cif (i <= -1)%1$c%2$c%2$creturn 0;%1$c%1$c%2$cduplicate(i);%1$c%2$ccompile(i);%1$c%2$cif (i > 0)%1$c%2$c%2$crun(i);%1$c}%1$c"
 
 static char buffer[256];
 
@@ -24,7 +24,7 @@ static void duplicate(int i)
 
 	FILE *f = fopen(buffer, "w");
 
-	fprintf(f, FORMAT, 10, 9, 34, FORMAT, i);
+	fprintf(f, FORMAT, 10, 9, 34, FORMAT, i - 1);
 	fclose(f);
 }
 
@@ -32,8 +32,8 @@ int main(void)
 {
 	int i = 5;
 
-	if (strcmp(__FILE__, "Sully.c") != 0)
-		i -= 1;
+	if (i <= -1)
+		return 0;
 
 	duplicate(i);
 	compile(i);
